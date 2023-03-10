7f5cadfdd92f6524068742cbca56f0ca53d6fa81
Estanislao Martinez Lacabe <emartinezlacabe@gmail.com>
2023-03-10 15:05:58 -0300

    sn

--- components/Footer.js
+++ components/Footer.js
@@ -7,12 +7,12 @@ const Footer = () => {
   return (
     <footer className="h-32 md:h-72 flex items-center justify-center text-slate-200 space-x-12">
 
-      <a target="_blank" rel="noreferrer" href='https://api.whatsapp.com/send/?phone=5493434653960' className='relative' aria-label="My Linkedin Profile" >
+      <a target="_blank" rel="noreferrer" href='https://www.linkedin.com/in/estanislao-martinez-lacabe-79a11b249/' className='relative' aria-label="My Linkedin Profile" >
         <FaLinkedin className='text-xl md:text-3xl' />
         <div className='w-6 h-6 absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 bg-blue-800 blur-xl -z-1 hover:bg-blue-500 transition-all ease-in-out duration-300 '></div>
       </a>
 
-      <a target="_blank" rel="noreferrer" href='https://api.whatsapp.com/send/?phone=5493434653960' className='relative' aria-label="My GitHub Profile">
+      <a target="_blank" rel="noreferrer" href='https://github.com/estaniml' className='relative' aria-label="My GitHub Profile">
         <FaGithubSquare className='text-xl md:text-3xl transition-all ease-in-out duration-200 ' />
         <div className='w-6 h-6 absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 bg-teal-800 blur-xl -z-1 hover:bg-teal-500 transition-all ease-in-out duration-300 '></div>
       </a>
